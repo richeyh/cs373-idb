303981b7a5a8fe2790fedcaa06e79029b97d5d69
Richard Huettel <richeyh@cs.utexas.edu>
2016-03-10 17:26:02 -0600

    updated models and tested api endpoints

--- models.py
+++ models.py
@@ -2,13 +2,26 @@ from extensions import DB
 
 class Author(DB.model):
 	#need to add fields below
-
-
-class Book(DB.model):
-	#need to add fields below
+	__tablename__="author"
+	name = DB.Column(DB.String(150))
 
 class Publisher(DB.model):
 	#need to add fields below
+	__tablename__="publisher"
+	name = DB.Column(DB.String(150))
+
+class Book(DB.model):
+	#need to add fields below
+	__tablename__="book "
+	isbn = DB.Column(DB.String(150))
+	title = DB.Column(DB.String(150))
+	summary = DB.Column(DB.Text())
+	price = DB.Column(DB.String(150))
+	best_seller_date = DB.Column(DB.date())
+	best_seller_list = DB.Column(DB.String(150))
+	book_image = DB.Column(DB.String(150))
+	author_id = DB.Column(DB.Integer, DB.ForeignKey(Author))
+	publisher_id = DB.Column(DB.Integer, DB.ForeignKey(Publisher))
 
 
 class TeamMember(DB.model):
