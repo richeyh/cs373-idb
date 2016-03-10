da9117b9fb3cd0cec50b7cd9dcb1d90210280db2
Richard Huettel <richeyh@cs.utexas.edu>
2016-03-10 17:31:11 -0600

    updated models for extra fields

--- models.py
+++ models.py
@@ -3,16 +3,25 @@ from extensions import DB
 class Author(DB.model):
 	#need to add fields below
 	__tablename__="author"
+	id = DB.Column(DB.Integer, primary_key=True)
 	name = DB.Column(DB.String(150))
+	book_count = DB.Column(DB.Integer)
+	best_seller_date = DB.Column(DB.date())
+	Books = DB.relationship("Book")
 
 class Publisher(DB.model):
 	#need to add fields below
 	__tablename__="publisher"
+	id = DB.Column(DB.Integer, primary_key=True)
 	name = DB.Column(DB.String(150))
+	location = DB.Column(DB.String(150))
+	book_count = DB.Column(DB.Integer)
+	Books = DB.relationship("Book")
 
 class Book(DB.model):
 	#need to add fields below
 	__tablename__="book "
+	id = DB.Column(DB.Integer, primary_key=True)
 	isbn = DB.Column(DB.String(150))
 	title = DB.Column(DB.String(150))
 	summary = DB.Column(DB.Text())
