9092eb2b377c679cb79122503d5b3356e8a150a1
Richard Huettel <richeyhuettel@yahoo.com>
2016-03-22 19:23:22 -0500

    updated about page to use members dict

--- views.py
+++ views.py
@@ -1,6 +1,15 @@
 from flask.views import MethodView
 from flask import render_template
 
+# temporary list for the no DB stage of this project
+members = {
+    "Richard": {"issues": 5, "commits": 5, "tests": 0, "resp": "backend and flask"},
+    "Rachel": {"issues": 5, "commits": 5, "tests": 0, "resp": "docker and uml"},
+    "Ruzseth": {"issues": 5, "commits": 5, "tests": 0, "resp": "documentation and html"},
+    "Timothy": {"issues": 5, "commits": 5, "tests": 0, "resp": "html and angular"},
+    "Kyung": {"issues": 5, "commits": 5, "tests": 0, "resp": "unit tests and apiuary"}
+}
+
 
 class IndexView(MethodView):
 
@@ -11,4 +20,4 @@ class IndexView(MethodView):
 class AboutView(MethodView):
 
     def get(self):
-        return render_template("about.html")
+        return render_template("about.html", members=members)
