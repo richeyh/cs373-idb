1ba49c156e2073f69d0cb5c97ffb3c86f9c594dd
Richard Huettel <richeyhuettel@yahoo.com>
2016-03-22 19:13:31 -0500

    Added about view

--- blueprints.py
+++ blueprints.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
-from views import IndexView
+from views import IndexView, AboutView
 
 blueprint = Blueprint('idb', __name__, template_folder='templates')
 
 
 blueprint.add_url_rule('/', view_func=IndexView.as_view('Home'))
+blueprint.add_url_rule('/about', view_func=AboutView.as_view('About'))
--- views.py
+++ views.py
@@ -4,5 +4,11 @@ from flask import render_template
 
 class IndexView(MethodView):
 
-	def get(self):
-		return render_template("home.html")
+    def get(self):
+        return render_template("home.html")
+
+
+class AboutView(MethodView):
+
+    def get(self):
+        return render_template("about.html")
