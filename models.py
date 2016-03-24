a3e62ac9f1e41dd03676d50243f58955a1a828c8
Richard Huettel <richeyhuettel@yahoo.com>
2016-03-24 17:49:43 -0500

    updated docstring in models.py to be pydoc compliant

--- models.py
+++ models.py
@@ -3,12 +3,16 @@ from extensions import DB
 
 class Author(DB.Model):
     """
-    id: primary key in the database
-    first_name: the first name of the author
-    last_name: the last name of the author
-    book_count: # of books in the database by the author
-    best_seller_date: last date of best seller by author
-    Books: all books the author wrote
+    An author of a book.
+
+    Attributes:
+        id                  primary key in the database
+        first_name          the first name of the author
+        last_name           the last name of the author
+        book_count          # of books in the database by the author
+        best_seller_date    last date of best seller by author
+        Books               all books the author wrote
+        Link                Link to the authors facebook page
     """
     __tablename__ = "author"
     id = DB.Column(DB.Integer, primary_key=True)
@@ -17,20 +21,24 @@ class Author(DB.Model):
     book_count = DB.Column(DB.Integer)
     best_seller_date = DB.Column(DB.Date())
     Books = DB.relationship("Book")
+    link = DB.Column(DB.String(256))
 
 
 class Book(DB.Model):
     """
-    id: primary key for the book object
-    isbn: the isbn # for the book
-    title: title of the book
-    summary: summary of the book by new york times
-    best_seller_date: date it made best seller best_seller_list
-    best_seller_list: the list or category it made the list for
-    book_image: url to the image for the book
-    author_id: primary key to the books author for linking
-    publisher: publisher who published the book
-    author: author who wrote the book
+    A Book object to hold information.
+
+    Attributes:
+        id                  primary key for the book object
+        isbn                the isbn # for the book
+        title               title of the book
+        summary             summary of the book by new york times
+        best_seller_date    date it made best seller best_seller_list
+        best_seller_list    the list or category it made the list for
+        book_image          url to the image for the book
+        author_id           primary key to the books author for linking
+        publisher           publisher who published the book
+        author              author who wrote the book
     """
     __tablename__ = "book"
     id = DB.Column(DB.Integer, primary_key=True)
@@ -47,14 +55,17 @@ class Book(DB.Model):
 
 class TeamMember(DB.Model):
     """
-    id: primary key for TeamMember
-    image_url: url of image for picture
-    name: name of user
-    bio: biography for user
-    resp: responsibilities
-    issue: # of issues created
-    commits: # of commits
-    tests: # of tests written
+    A TeamMember object for use latter to track changing statistics.
+
+    Attributes:
+        id          primary key for TeamMember
+        image_url   url of image for picture
+        name        name of user
+        bio         biography for user
+        resp        responsibilities
+        issue       # of issues created
+        commits     # of commits
+        tests       # of tests written
     """
     id = DB.Column(DB.Integer, primary_key=True)
     image_url = DB.Column(DB.String(150))
