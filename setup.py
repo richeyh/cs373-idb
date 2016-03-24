defaddc019b4094c98da7afbeaeafeebfbbcf9bd
Richard Huettel <richeyhuettel@yahoo.com>
2016-03-24 18:58:19 -0500

    removed readme req from setup.py

--- setup.py
+++ setup.py
@@ -8,16 +8,11 @@ if os.getenv('VBOX_MOUNTED'):
 from setuptools import setup
 from setuptools import find_packages
 
-
-with open('README.rst', 'r') as readmefile:
-
-   README = readmefile.read()
-
 setup(
    name='IBDB',
    version='0.1.0',
    url='https://github.com/richeyh/cs373-idb.git',
-   description='we store books',
+   description='we store books and authors',
    packages=find_packages(exclude=['tests', 'build', 'dist', 'docs']),
    install_requires=[
        'flask'
