981e0decc6738b34b7f6fad2ae3fee5df2450ae9
Richard Huettel <richeyhuettel@yahoo.com>
2016-04-05 19:13:02 -0500

    added host detection for config.py

--- app/config.py
+++ app/config.py
@@ -1,14 +1,18 @@
 import os
+import socket
 
-SQLALCHEMY_DATABASE_URI = \
-    '{engine}://{username}:{password}@{hostname}/{database}'.format(
-        engine='mysql',
-        username=os.getenv('MYSQL_USER'),
-        password=os.getenv('MYSQL_PASSWORD'),
-        hostname=os.getenv('MYSQL_HOST'),
-        database=os.getenv('MYSQL_DATABASE'))
+if socket.gethostname() == "localhost.localdomain":
+    SQLALCHEMY_DATABASE_URI = "mysql://ibdb:ibdb@localhost/ibdb"
+else:
+    SQLALCHEMY_DATABASE_URI = \
+        '{engine}://{username}:{password}@{hostname}/{database}'.format(
+            engine='mysql',
+            username=os.getenv('MYSQL_USER'),
+            password=os.getenv('MYSQL_PASSWORD'),
+            hostname=os.getenv('MYSQL_HOST'),
+            database=os.getenv('MYSQL_DATABASE'))
 
 # set this to false in production
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 DEBUG = True
-SECRET_KEY="random string of stuff book SWEEEPIMMG ASDFADFqkwehjrklhq	l;falsekl	jewri;	`po"
+SECRET_KEY = "random string of stuff book SWEEEPIMMG ASDFADFqkwehjrklhq	l;falsekl	jewri;	`po"
