dfc1a7dc1512827a912c944777113ed54f4a9a80
Richard Huettel <richeyh@cs.utexas.edu>
2016-04-07 15:01:51 -0500

    added query terms

--- app/fetch_data.py
+++ app/fetch_data.py
@@ -135,7 +135,7 @@ for category in lists:
             lastName = "None"
         firstName = single_author.split(' ')[0]
         with app.app_context():
-            author = Author.query.filter_by(first_name==firstName).filter_by(last_name==lastName).all()[0]
+            author = Author.query.filter_by(Author.first_name==firstName).filter_by(Author.last_name==lastName).all()[0]
         if author:
             book_author = author
         else:
@@ -144,7 +144,7 @@ for category in lists:
                 DB.session.add(book_author)
                 DB.session.commit()
         with app.app_context():
-            q_book = Book.query.filter_by(isbn==book_isbn).all()[0]
+            q_book = Book.query.filter_by(Book.isbn==book_isbn).all()[0]
         if q_book:
             b = q_book
             b.best_seller_list = b.best_seller_list+", "+book_best_seller_list
